51d84a53e4138749f689dd422dedc66ba25021a6
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-05-23 17:57:56 +0200

    Fix

--- src/main/java/com/company/enroller/model/Participant.java
+++ src/main/java/com/company/enroller/model/Participant.java
@@ -1,33 +1,22 @@
 package com.company.enroller.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "participant")
 public class Participant {
 
-	@Id
-	private String login;
+    @Id
+    private String login;
 
-	@Column
-	private String password;
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @Column
+    private String password;
 }
